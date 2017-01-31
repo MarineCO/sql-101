7983462c481e5b88fe835a9a8a2b8c32963df5f4
marineco <marinecolonge@gmail.com>
2017-01-31 14:22:55 +0100

    requête avec PDO

--- index_pdo.php
+++ index_pdo.php
@@ -4,3 +4,24 @@ $bdd = new PDO(
 	'mysql:host=localhost;dbname=mon_armoire;charset=utf8',
 	'root', 'root');
 
+$reponse = $bdd->query('SELECT * FROM mes_chaussettes');
+
+$donnees = $reponse->fetch();
+
+while ($donnees = $reponse->fetch()) {
+
+?>
+	<p>
+		id : <?= $donnees['id']; ?><br />
+		couleur : <?= $donnees['couleur']; ?><br />
+		description : <?= $donnees['description']; ?><br />
+		pointure : <?= $donnees['pointure']; ?><br />
+		date_lavage : <?= $donnees['date_lavage']; ?><br />
+		temp_lavage	: <?= $donnees['temp_lavage']; ?><br />
+	</p>
+
+<?php
+
+}
+
+?>
