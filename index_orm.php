715a22c37cd9516e7935fcc65428f662882c2e5d
marineco <marinecolonge@gmail.com>
2017-01-31 19:22:44 +0100

    ajout du doctype

--- index_orm.php
+++ index_orm.php
@@ -10,24 +10,31 @@ $donnees = ORM::for_table('mes_chaussettes')
 ->where_gt('pointure', '40')
 ->find_many();
 
-?>
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercice BDD</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
 	<table>
+	
 		<tr>
 			<td>Id :</td>
 			<td>Couleur :</td>
 		</tr>
-<?php
 
-foreach ($donnees as $donnee) {
+<?php foreach ($donnees as $donnee) : ?>
 
-?>
 		<tr>
 			<td><?= $donnee->id; ?></td>
 			<td><?= $donnee->pointure; ?></td>
 		</tr>
-<?php
 
-}
+<?php endforeach; ?>
 
-?>
 	</table>
+
+</body>
+</html>
